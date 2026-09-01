aab4eeb0418efeefef4f06eaba568a1db9b68319
Purvi Aneja <anejapurvi2007@gmail.com>
2026-09-01 09:58:52 +0530

    Time: 0 ms (100%), Space: 13 MB (82.45%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in seen:
+                return [seen[complement], i]
+
+            seen[num] = i
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
